49a301a36fa73990d26d58eb438e08c0c4c962ee
dasunNimantha <dasunnimantha77@outlook.com>
2020-12-07 08:59:25 +0530

    sharing with collaborators

--- src/sample/controllers/Main.java
+++ src/sample/controllers/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/SignUp/SignUp.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/Login.fxml"));
 
         Scene scene = new Scene(root,1049, 594);
         primaryStage.setScene(scene);
