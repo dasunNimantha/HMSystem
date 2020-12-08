9de02279096c0b45b28ebabe0fa47507b66fe9b9
𝔸 𝕊 ℍ 𝔸 ℕ <atashan97@gmail.com>
2020-12-09 01:10:21 +0530

    Added new classes

---
+++ src/sample/models/Medicalofficer.java
@@ -0,0 +1,11 @@
+package sample.models;
+
+import java.time.LocalDate;
+
+public class Medicalofficer {
+
+    private int staffId;
+    private String staffEmailAddress;
+    private LocalDate dateOfJoin;
+
+}
