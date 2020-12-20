6289746ccd5a05ad60bc48183d55bb9706cd7f84
lpy <hashrnwr@gmail.com>
2020-12-20 10:47:31 +0530

    modified receptionist profile

--- src/sample/controllers/dashboardController/ReceptionistDash/ReceptionistController.java
+++ src/sample/controllers/dashboardController/ReceptionistDash/ReceptionistController.java
@@ -1,6 +1,9 @@
 package sample.controllers.dashboardController.ReceptionistDash;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,7 +11,8 @@ import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import sample.controllers.SceneLoader;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import java.io.IOException;
 
@@ -42,7 +46,44 @@ public class ReceptionistController {
     private AnchorPane step3Anchor;
 
     @FXML
+    private JFXComboBox<String> dropGender;
+
+    @FXML
+    private JFXComboBox<String> dropMaritial;
+
+    @FXML
     private BorderPane recepBorderPane;
+
+    @FXML
+    private JFXTextField textSID;
+
+    @FXML
+    private JFXButton editSettings;
+
+    @FXML
+    private JFXTextField textUserName;
+
+    @FXML
+    private JFXTextField textDj;
+
+    @FXML
+    private JFXTextField textEmail;
+
+    @FXML
+    private JFXTextField textID;
+
+    @FXML
+    private JFXTextField textPhone;
+
+    @FXML
+    private JFXTextArea textAddress;
+
+    @FXML
+    private JFXTextField textDOB;
+
+    @FXML
+    private JFXTextField textName;
+
 
     @FXML
     void disList(MouseEvent event) {
@@ -58,6 +99,7 @@ public class ReceptionistController {
     void addVisLoad(MouseEvent event) {
 
     }
+
 
     @FXML
     void step1(ActionEvent event) throws IOException {
@@ -97,18 +139,32 @@ public class ReceptionistController {
     }
 
     @FXML
-    void step7(ActionEvent event) {
-
-    }
-
-    @FXML
     void test(ActionEvent event) throws IOException {
 
 
     }
+    @FXML
+    void profileEditable(MouseEvent event) {
+        textID.setEditable(true);
+        textSID.setEditable(true);
+        textDOB.setEditable(true);
+        textAddress.setEditable(true);
+        textDj.setEditable(true);
+        textEmail.setEditable(true);
+        textName.setEditable(true);
+        textPhone.setEditable(true);
+        textUserName.setEditable(true);
+        editSettings.setText("Save Changes");
 
 
+    }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //dropMaritial.getItems().removeAll(dropMaritial.getItems());
+        dropMaritial.getItems().add("Married");
+        dropMaritial.getItems().add("UnMarried");
+    }
 
 
 
